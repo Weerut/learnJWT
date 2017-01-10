89d8eaa25df871593fe04d44420d1085f87fdc2e
weerut panjawattana <weerut.pan@gmail.com>
2017-01-10 20:16:01 +0700

    a

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
-
+    'database': 'mongodb://test:test@ds161038.mlab.com:61038/jwttest'
 };
