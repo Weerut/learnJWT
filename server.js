b2eefa22c3c2b95771c4ef95fa47d5ffb85c9543
Weerut Panjawattana <Weerut Panjawattana>
2017-01-10 23:25:46 +0700

    upload

--- server.js
+++ server.js
@@ -62,6 +62,60 @@ var apiRoutes = express.Router();
 
 // TODO: route middleware to verify a token
 
+
+
+apiRoutes.post('/authenticate', function (req, res) {
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+        if (err) throw err;
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+                // create token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: 1440 // 24 hours
+                });
+
+                // return token to requester
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token',
+                    token: token
+                });
+            }
+        }
+    });
+})
+
+apiRoutes.use(function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        return res.status(403).send(
+            {
+                success: false,
+                message: 'No token provided.'
+            }
+        )
+    }
+
+
+
+
+});
+
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function (req, res) {
     res.json({ message: 'Welcome to the coolest API on earth!' });
